38d325d79a8c3b6799f4cbe6efa7287eda4cf761
Raunak Warrier <raunakwarrier@outlook.com>
2026-04-01 23:49:35 +0530

    added spring security

---
+++ syncspace-backend/src/main/java/com/syncpace/backend/security/JwtUtils.java
@@ -0,0 +1,40 @@
+package com.syncpace.backend.security;
+
+import java.security.Key;
+import java.util.Date;
+
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtils {
+    private final Key jwtSecretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final int jwtExpirationMs = 86400000; // 24 hours
+
+    public String generateJwtToken(String username) {
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(jwtSecretKey)
+                .compact();
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parser().verifyWith((javax.crypto.SecretKey) jwtSecretKey).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().verifyWith((javax.crypto.SecretKey) jwtSecretKey).build().parseSignedClaims(authToken);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            System.err.println("Invalid JWT token: " + e.getMessage());
+        }
+        return false;
+    }
+}
